a6a884b2d5ee2ea0d5ea19ecf87101db56278e74
Douglas Fabiano <douglasfabiano23@gmail.com>
2023-03-27 18:53:14 -0300

    Ex2

---
+++ rotas-ex2/src/routes/Subscription/index.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import Button from "../../components/Button";
+import Card from "../../components/Card";
+
+
+export default function Subscription() {
+
+    return (
+        <main>
+            <section>
+                <div className="container">
+                    <h2 className="section-title">Faça sua Inscrição</h2>
+                    <div className="mt30 mb30">
+                        <Card title="Página de Inscrição" />
+                    </div>
+                    <div className="dflex">
+                        <Link to="promotion">
+                            <Button text="Ver promoção" />
+                        </Link>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
+}
