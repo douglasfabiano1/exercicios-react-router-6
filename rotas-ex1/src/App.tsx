717d3aabd8db4853e78bc07b390c53aae9f8f718
Douglas Fabiano <douglasfabiano23@gmail.com>
2023-03-27 13:07:20 -0300

    Basic route structure

--- rotas-ex1/src/App.tsx
+++ rotas-ex1/src/App.tsx
@@ -1,8 +1,18 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import HomeBody from "./routes/Home/HomeBody";
+import Promotion from "./routes/Home/Promotion";
+
+export default function App() {
 
   return (
-    <h1>MeuSite</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<HomeBody />} />
+          <Route path="promotion" element={<Promotion />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
